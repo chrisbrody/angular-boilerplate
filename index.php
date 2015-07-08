ee90eb084835929039b42659a9c1fc6c7c0aef67
Chris Brody <chrisbrody@codifyacademy.com>
2015-07-08 10:23:04 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- character set -->
+	<meta charset="utf-8" />
+	<!-- author -->
+	<meta name="author" content="Chris Brody" />
+	<!-- description -->
+	<meta name="description" content="An angular testing site">
+	<!-- keywords -->
+	<meta name="keywords" content="Angular Testing">
+	<!-- title -->
+	<title>Angular Testing</title>
+	<!-- bootstrap css -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- custom css -->
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body ng-app="myApp">
+
+	<h1>Angular Boiler Plate</h1>
+
+
+	<!-- jQuery js -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- bootstrap js-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<!-- angular js -->
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+	<!-- angular app js -->
+	<script src="app/app.js"></script>
+	<!-- custom js -->
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,1 @@
+console.log('main.js page line 1');
